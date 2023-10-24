065cbaa0c074b0c573f86d6fa0e20cc622a6922a
FilbertKimasa <filbertdan67@gmail.com>
2023-10-24 17:47:58 +0300

    Add text decoration

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,14 +5,14 @@ import '../styles/Header.css';
 
 function Header() {
   return (
-    <header className="panel-bg">
-      <div className="head">
+    <header className="display-flex">
+      <div className="head display-flex">
         <img src={logo} alt="logo" className="logo" />
         <span className="app-name">Space Traveler&lsquo;s Hub</span>
       </div>
 
       <nav>
-        <ul className="nav-list">
+        <ul className="nav-list display-flex">
           <li>
             <Link to="/Rocket" className="rocket-link">
               Rocket
