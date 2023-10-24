04c1529a628d604ece1e4caefdec074faadd04ef
Surafels <ssurafel4@gmail.com>
2023-10-24 20:38:47 +0300

    Update nav list color and fix errors

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import logo from '../assets/logo.png';
 import '../styles/Header.css';
 
