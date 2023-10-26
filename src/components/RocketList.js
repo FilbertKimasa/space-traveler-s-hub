04784c5c406dc27f23e047c4713b338ccf12ee1a
FilbertKimasa <filbertdan67@gmail.com>
2023-10-26 03:04:28 +0300

    Remove commented code

--- src/components/RocketList.js
+++ src/components/RocketList.js
@@ -1,11 +1,9 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-// import { useSelector } from 'react-redux/es/hooks/useSelector';
 import { fetchRockets } from '../redux/rockets/rocketsSlice';
 
 function RocketList() {
   const dispatch = useDispatch();
-  // const rockets = useSelector((state) => state.rockets);
   useEffect(() => {
     dispatch(fetchRockets());
   }, [dispatch]);
