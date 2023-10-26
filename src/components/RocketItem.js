fbfdb9932f424170becd3d0f3529b5cbd9724ca0
FilbertKimasa <filbertdan67@gmail.com>
2023-10-26 13:35:20 +0300

    Pass item to the rocket item

--- src/components/RocketItem.js
+++ src/components/RocketItem.js
@@ -1,7 +1,22 @@
+/* eslint-disable react/button-has-type */
+/* eslint-disable jsx-a11y/control-has-associated-label */
+/* eslint-disable react/jsx-no-comment-textnodes */
+/* eslint-disable react/prop-types */
 import React from 'react';
 
-function RocketItem() {
-  return <></>;
+function RocketItem({ itemProp }) {
+  return (
+    <>
+      <li>
+        <img src={itemProp.flickr_images[0]} alt="rocket" />
+        <span>
+          <h2>{itemProp.name}</h2>
+          <p>{itemProp.description}</p>
+          <button>Reserve Rocket</button>
+        </span>
+      </li>
+    </>
+  );
 }
 
 export default RocketItem;
--- src/components/RocketList.js
+++ src/components/RocketList.js
@@ -1,13 +1,28 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux/es/hooks/useSelector';
 import { fetchRockets } from '../redux/rockets/rocketsSlice';
+// eslint-disable-next-line no-unused-vars
+import RocketItem from './RocketItem';
 
 function RocketList() {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchRockets());
   }, [dispatch]);
-  return <></>;
+  // eslint-disable-next-line no-unused-vars
+  const rockets = useSelector((state) => state.rockets.rockets);
+  // console.log(rockets);
+  return (
+    <>
+      {/* <RocketItem key={book.item_id} itemProp={book} />; */}
+      <ul>
+        {rockets.map((rocket) => (
+          <RocketItem key={rocket.id} itemProp={rocket} />
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default RocketList;
