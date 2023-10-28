e1e68e60b57dcf5acf9932db440efa940873402c
FilbertKimasa <filbertdan67@gmail.com>
2023-10-28 07:36:07 +0300

    Create Profile component

---
+++ src/components/Profle.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Profle() {
+  return (
+    <section className="profile-section">
+      <div>My Mission</div>
+      <div>My Rockets</div>
+    </section>
+  );
+}
+
+export default Profle;
