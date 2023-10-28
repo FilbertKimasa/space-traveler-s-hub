b168a3bcb221c510b1d37adba41d9a4b5388964f
FilbertKimasa <filbertdan67@gmail.com>
2023-10-28 07:50:51 +0300

    Add profile.css file

--- src/components/Profle.js
+++ src/components/Profle.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 function Profle() {
+  // eslint-disable-next-line no-unused-vars
+  const rockets = useSelector((state) => state.rockets.rockets);
   return (
     <section className="profile-section">
       <div>My Mission</div>
