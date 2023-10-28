8d063ef8102c0d07accb58064b4385ec22c81629
FilbertKimasa <filbertdan67@gmail.com>
2023-10-28 17:20:55 +0300

    Modify profile component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import MissionList from './components/mission';
 import RocketList from './components/RocketList';
-import Profile from './components/profile';
+import Profle from './components/Profle';
 import './styles/App.css';
 
 function App() {
@@ -13,7 +13,7 @@ function App() {
         <Route path="/" element={<RocketList />} />
         <Route path="/rockets" element={<RocketList />} />
         <Route path="/mission" element={<MissionList />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/profile" element={<Profle />} />
       </Routes>
     </>
   );
--- src/components/Profle.js
+++ src/components/Profle.js
@@ -6,10 +6,23 @@ function Profle() {
   const rockets = useSelector((state) => state.rockets.rockets).filter(
     (rocket) => rocket.reserved,
   );
+  const { missions } = useSelector((state) => state.missions);
+  const missionJoined = missions.filter((mission) => mission.join);
 
   return (
     <section className="profile-section">
-      <div>My Mission</div>
+      <div>
+        <h2>My Mission</h2>
+        {missionJoined.length > 0 ? (
+          <ul className="rocket-reserved-list">
+            {missionJoined.map((mission) => (
+              <li key={mission.mission_id}>{mission.mission_name}</li>
+            ))}
+          </ul>
+        ) : (
+          <p className="notJoined">There is no joined mission.</p>
+        )}
+      </div>
       <div>
         <h2>My Rockets</h2>
         <ul className="rocket-reserved-list">
